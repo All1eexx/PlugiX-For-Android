55d07fe17097a0bbc0aeaf323e92e319d3048afd
All1eexx <174635518+All1eexx@users.noreply.github.com>
2025-08-09 20:11:52 +0200

    - added template for java

---
+++ plugins/.create_plugin_project/templates/java/PLUGINNAME.java
@@ -0,0 +1,12 @@
+package com.all1eexxx;
+
+import android.util.Log;
+import android.content.Context;
+
+public class PLUGINNAME {
+  private static final String TAG = "PLUGINNAME";
+
+  public static void OnPluginCreate(Context ctx) {
+    Log.i(TAG, "Hello World from PLUGINNAME plugin!");
+  }
+}
